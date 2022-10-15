d372ae574dca303e565053181554aebeee286b60
gramseyer <ramseyer@uchicago.edu>
2022-10-15 05:30:25 -0700

    iface

--- include/utils/mkdir.h
+++ include/utils/mkdir.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <string>
+
 namespace utils
 {
 
 bool 
 mkdir_safe(const char* dirname);
+
+bool
+mkdir_safe(const std::string& dirname)
+{
+	return mkdir_safe(dirname.c_str());
+}
 
 } /* utils */
