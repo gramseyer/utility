52fb3386ab3282d507e1828bb7d0b1da2e41de54
Geoff Ramseyer <gdwr@cs.stanford.edu>
2023-11-19 16:34:38 -0800

    filesystem clear

--- include/utils/mkdir.h
+++ include/utils/mkdir.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
 
 namespace utils
 {
@@ -13,6 +14,17 @@ static bool
 mkdir_safe(const std::string& dirname)
 {
 	return mkdir_safe(dirname.c_str());
+}
+
+[[maybe_unused]]
+static void
+clear_directory(const std::string& dirname)
+{
+	std::error_code ec;
+	std::filesystem::remove_all({dirname}, ec);
+	if (ec) {
+		throw std::runtime_error("failed to clear directory");
+	}
 }
 
 } /* utils */
