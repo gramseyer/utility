bb3d152d33b59b8ee476039e4a6ab646695dede4
Geoff Ramseyer <geoff.ramseyer@cs.stanford.edu>
2024-04-24 12:01:22 -0700

    std unreachable

--- include/utils/compat.h
+++ include/utils/compat.h
@@ -1,10 +1,24 @@
 #pragma once
 
-static inline void SPINLOCK_PAUSE() {
-#if __has_builtin(__builtin_ia32_pause)
-    __builtin_ia32_pause();
+#include <utility>
+
+static inline void SPINLOCK_PAUSE()
+{
+    #if __has_builtin(__builtin_ia32_pause)
+        __builtin_ia32_pause();
+    #elif defined(__aarch64__)
+        asm volatile ("yield");
+    #endif
 }
-#elif defined(__aarch64__)
-    asm volatile ("yield");
+
+#if __cpp_lib_unreachable > 202202L
+void unreachable() {
+    std::unreachable();
+}
+#else
+void unreachable() {
+    perror("unreachable!");
+    std::abort();
 }
 #endif
+
